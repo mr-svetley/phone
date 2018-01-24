8352d1ada77fa5024ae2d91b2243bb4fd3ebad6b
mr-svetley <mr.svetley@gmail.com>
2018-01-24 19:49:57 +0500

    init

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var phoneWidget = (function(){
+  
+  var widget = document.querySelector('#phone-widget');
+  var widget = document.querySelector('body'); /*Для блоков-презентаций, по завершению разработки удалить*/
+  
+  var scrollerWrapper = widget.querySelectorAll('.js-pw-scroller-wrapper');
+  var scroller;
+  
+  for (var i = 0; i < scrollerWrapper.length; i++) {
+    
+    scroller = scrollerWrapper[i].querySelector('.js-pw-scroller-wrapper > .js-pw-scroller');
+    
+    if ( scroller ) {
+      scrollerWrapper[i].style.overflowX = 'hidden';
+      scroller.style.width = scrollerWrapper[i].clientWidth + ( scroller.offsetWidth - scroller.clientWidth  ) + 'px';
+    }
+  }
+
+  scrollerWrapper = null;
+  scroller = null;
+
+  return false;
+})();
